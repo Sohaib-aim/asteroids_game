412c28c3c979701d15e1a1e9d237927c22d47e29
Sohaib-aim <sohaibamirdevlop@gmail.com>
2026-07-17 18:45:22 +0500

    added fps

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
+
         for event in pygame.event.get():
             screen.fill("black")
             pygame.display.flip()
+
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
