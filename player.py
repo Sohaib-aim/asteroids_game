71f7cc73ecf06adf00ec0e5757e05739784bb1ed
Sohaib-aim <sohaibamirdevlop@gmail.com>
2026-07-18 09:53:36 +0500

    applied rate limit on bullets

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 import pygame
 from shot import Shot
 
@@ -7,6 +7,8 @@ class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown_timer = 0
+
     
     # in the Player class
     def triangle(self) -> list[pygame.Vector2]:
@@ -32,6 +34,7 @@ class Player(CircleShape):
 
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
+        self.shot_cooldown_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -42,6 +45,10 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
+            if self.shot_cooldown_timer > 0:
+                return
+            if self.shot_cooldown_timer <= 0:
+                self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
                 self.shoot()
     
     def shoot(self):
